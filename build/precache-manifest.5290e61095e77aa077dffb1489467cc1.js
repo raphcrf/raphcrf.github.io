f4a585505b4224fa9c683a8cc2a72dcbfcc777d1
Raphael Abdias <rmacrf@gmail.com>
2019-07-30 17:47:07 -0300

    First test of the Git Pages

---
+++ build/precache-manifest.5290e61095e77aa077dffb1489467cc1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9485900c20f4bdcc2c3560ce45b99935",
+    "url": "/CM-react/index.html"
+  },
+  {
+    "revision": "97485f66f1fd8b16494d",
+    "url": "/CM-react/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/CM-react/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "97485f66f1fd8b16494d",
+    "url": "/CM-react/static/js/main.4ac2ecb8.chunk.js"
+  },
+  {
+    "revision": "0ae74deb0dd2f9a7e9a8",
+    "url": "/CM-react/static/js/runtime~main.45e807f7.js"
+  }
+]);
